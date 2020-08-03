a5f2ebb9c207394e16203aa14643ec80943ac1f2
ivano-vvv <ivanovlad2010@gmail.com>
2020-08-03 12:01:04 +0300

    object with console commands created

---
+++ consoleMessages.js
@@ -0,0 +1,31 @@
+const consoleMessages = {
+  userConnected(id) {
+    this._log(`a user ${id} connected`);
+  },
+  roomCreated(username, userId, roomname, roomId) {
+    this._log(
+      `user ${username} (${userId}) created a Room named "${roomname}" (${roomId})`
+    );
+  },
+  userJoinedRoom(username, userId, roomname, roomId) {
+    this._log(
+      `user ${username} (${userId}) joined the Room "${roomname}" (${roomId})`
+    );
+  },
+  userGotRoomData(username, userId, roomname, roomId) {
+    this._log(
+      `user ${username} (${userId}) have got data about the Room "${roomname}" (${roomId})`
+    );
+  },
+  userLeftRoom(username, userId, roomname, roomId) {
+    this._log(
+      `user ${username} (${userId}) left the Room "${roomname}" (${roomId})`
+    );
+  },
+
+  _log(m) {
+    console.log(m);
+  },
+};
+
+module.exports = consoleMessages;
