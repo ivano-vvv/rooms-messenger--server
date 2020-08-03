045783ae7c143d2f896e4299f0ba4b1fda8804fd
ivano-vvv <ivanovlad2010@gmail.com>
2020-08-03 09:18:19 +0300

    removing user after disconnection added

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const io = require("socket.io")(http);
 const port = process.env.PORT || 5000;
 
 const data = require("./data/rooms");
+const { getRoomById } = require("./data/rooms");
 
 io.on("connection", (socket) => {
   let userId = socket.id;
+  let userRoom = "";
   console.log("user connected");
 
   socket.on("createRoom", ({ creatorName, roomName }) => {
     let roomId = data.createRoom(creatorName, roomName, userId);
+    userRoom = roomId;
     socket.join(roomId);
     let room = data.getRoomById(roomId);
     console.log(
@@ -18,15 +21,22 @@ io.on("connection", (socket) => {
     );
 
     io.to(roomId).emit("roomData", { room, userData: room.users[0] });
+    socket.emit("connectionData", {
+      userId,
+      roomId: room.id,
+      username: room.users[0].name,
+    });
     console.log(
       `user ${creatorName} (${room.users[0].id}) get data about the Room "${roomName}" (${room.id})`
     );
   });
 
-  socket.on("joinRoom", ({ roomId, userName }) => {
+  socket.on("joinRoom", ({ roomId, username }) => {
     let room = data.getRoomById(roomId);
     if (room) {
-      room.addUser(userName, userId);
+      userRoom = roomId;
+
+      room.addUser(username, userId);
 
       io.to(roomId).emit("newUser", {
         message: room.getLastMessage(),
@@ -51,6 +61,34 @@ io.on("connection", (socket) => {
     }
   });
 
+  // socket.on("rejoinRoom", ({ userId, username, roomId }) => {
+  //   let room = data.getRoomById(roomId);
+  //   if (room) {
+  //     room.addUser(username, userId);
+
+  //     io.to(roomId).emit("newUser", {
+  //       message: room.getLastMessage(),
+  //       users: room.users,
+  //     });
+
+  //     socket.join(roomId);
+
+  //     console.log(
+  //       `user ${room.users[room.users.length - 1].name} (${
+  //         room.users[room.users.length - 1].id
+  //       }) joined the Room "${room.name}" (${room.id})`
+  //     );
+
+  //     socket.emit("roomData", { room, userData: room.getLastUser() });
+
+  //     console.log(
+  //       `user ${room.users[room.users.length - 1].name} (${
+  //         room.users[room.users.length - 1].id
+  //       }) get data about the Room "${room.name}" (${room.id})`
+  //     );
+  //   }
+  // });
+
   socket.on("leaveRoom", ({ roomId, userId }) => {
     let room = data.getRoomById(roomId);
     if (room) {
@@ -58,6 +96,7 @@ io.on("connection", (socket) => {
 
       socket.emit("logOut");
       socket.leave(roomId);
+      userRoom = "";
 
       room.removeUser(userId);
 
@@ -81,7 +120,13 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("a user disconnected");
+    let room = data.getRoomById(userRoom);
+
+    room.removeUser(userId);
+    io.to(room.id).emit("removeUser", {
+      message: room.getLastMessage(),
+      users: room.users,
+    });
   });
 });
 
